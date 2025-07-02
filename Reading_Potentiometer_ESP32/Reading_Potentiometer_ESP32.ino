39ee6fb5fc3f8814b99666cbb094998974a197a3
GAD-18 <gideonansahdebrah17@gmail.com>
2025-07-02 01:33:42 +0000

    Add files via upload

---
+++ Reading_Potentiometer_ESP32/Reading_Potentiometer_ESP32.ino
@@ -0,0 +1,21 @@
+const int potPin = 15;
+const int ledPin = 3;
+
+void setup() {
+   pinMode(ledPin, OUTPUT);
+   Serial.begin(115200);
+}
+
+void loop() {
+  int potValue = analogRead(potPin);
+  int brightness = map(potValue, 0, 4095, 0, 1023);
+
+  for ( int i = 0; i < 50; i++) {
+    digitalWrite(ledPin, HIGH);
+    delayMicroseconds(brightness);
+    digitalWrite(ledPin, LOW);
+    delayMicroseconds(1023 - brightness);
+  }
+
+  Serial.println(brightness);
+}
